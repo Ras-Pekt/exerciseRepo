14a011747eef8fcb0d6f26cd142e5dd9aba1b819
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-04-02 16:52:03 +0300

    hour 4 simple exercises

---
+++ c/hour_4/hr4ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num_z = 'z';
+	int num_Z = 'Z';
+	
+	printf("The numeric value of z and Z is %d and %d\n", num_z, num_Z);
+	return (0);
+}
---
+++ c/hour_4/hr4ex2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char num1 = 72;
+	char num2 = 104;
+	
+	printf("The character represented by 72 and 104 is %c and %c\n", num1, num2);
+	return (0);
+}
